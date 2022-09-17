bb873d5687f4e6fb2241198ea46211d845bce352
CodeConnoisseur74 <sherry.andrews@outlook.com>
2022-09-17 08:40:28 +0000

    initial commit and add functions structure

--- run.py
+++ run.py
@@ -1,3 +1,24 @@
-# Your code goes here.
-# You can delete these comments, but do not change the name of this file
-# Write your code to expect a terminal of 80 characters wide and 24 rows high
+#Legend 
+# X for placing the battleship and for hitting the battleship
+# ' ' for available space 
+# '_' for missed shot 
+
+
+from random import randint
+
+HIDDEN_BOARD = [[''] * 8 for x in range(8)]
+GUESS_BOARD = [[''] * 8 for x in range(8)]
+
+letters_to_numbers = {'A': 0, 'B': 1, 'C': 2, 'D': 3, 'E': 4, 'F': 5, 'G': 6, 'H': 7}
+
+def board():
+    pass 
+
+def create_ship():
+    pass
+
+def get_ship_location():
+    pass
+
+def count_hit_ships():
+    pass
